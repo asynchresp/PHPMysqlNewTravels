144ceac9f6cc580c00ba3df9d3ad1f244947a07c
Avin D'Silva <avindsilva@gmail.com>
2014-03-22 21:24:39 +0530

    Fixed the header and footer

--- header.php
+++ header.php
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-<link href="style.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-<div id="header">
-      <h1 style="text-align:center;">New Travels Pvt. Ltd. </h1>
-    </div>
-</body>
-=======
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -26,5 +11,5 @@
       <h1 style="text-align:center;">Royal Travel and Tours, Seeb</h1>
     </div>
 </body>
->>>>>>> 7a3578bf9335b01600a2dd01c49a1362e38717a7
+
 </html>
